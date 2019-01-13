81b7ce665b0f23bb6bb97800073a8798d405fdee
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 18:32:53 -0200

    added actions and flux was fully implemented

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,8 +1,10 @@
 import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
 
 
 export default class NoteStore {
   constructor() {
+    this.bindActions(NoteActions);
     this.notes = [
       {
         id: uuid.v4(),
@@ -13,5 +15,28 @@ export default class NoteStore {
         task: 'Do Laundry'
       }
     ]
+  }
+
+  create(note){
+    this.setState({
+      notes: this.notes.concat(note)
+    });
+  }
+
+  update(updateNote){
+    this.setState({
+      notes: this.notes.map(note => {
+        if(note.id === updateNote.id) {
+          return Object.assign({}, note, updateNote);
+        }
+        return note
+      })
+    });
+  }
+
+  delete(id){
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id )
+    })
   }
 }
