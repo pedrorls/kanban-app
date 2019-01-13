cfba378dad066ca53fd3590f4bc8d2ff8484fc82
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 21:11:26 -0200

    added some style and deleted unecessary comments

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -14,7 +14,7 @@ const App = ({ LaneActions, lanes }) => {
 
   return (
     <div>
-      <button className='add-lane' onClick={ addLane }>+</button>
+      <button className='add-lane' onClick={ addLane }>+ Add Lane</button>
       <Lanes lanes={ lanes }/>
     </div>
   )
@@ -25,46 +25,3 @@ export default connect(({ lanes }) => ({
 }), {
   LaneActions
 })(App)
-// class App extends Component{
-
-//   addNote = () => {
-//     this.props.NoteActions.create({
-//       id: uuid.v4(),
-//       task: 'New Task'
-//     })
-//   }
-
-//   deleteNote = (id, e) => {
-//     e.stopPropagation();
-//     this.props.NoteActions.delete(id)
-//   }
-
-//   activateNoteEdit = (id) => {
-//     this.props.NoteActions.update({ id, editing: true });
-//   }
-
-//   editNote = (id, task) => {
-//     this.props.NoteActions.update({ id, task, editing: false});
-//   }
-
-//   render() {
-//     const { notes } = this.props;
-//     return(
-//       <div>
-//         <button className="add-note" onClick={ this.addNote }>+</button>
-//         <Notes 
-//           notes={ notes }
-//           onDelete={ this.deleteNote }
-//           onNoteClick={ this.activateNoteEdit }
-//           onEdit={ this.editNote }
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default connect(({ notes }) => ({
-//   notes
-// }), {
-//   NoteActions
-// })(App)
