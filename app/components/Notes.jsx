37079118e4266d3ebb215e19d26cfddfbbb19a34
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 00:11:48 -0200

    separeted in smaller components

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import Note from './Note';
 
-export default ({notes}) => (
-  <ul>{ notes.map(note => <li key={note.id}>{ note.task }</li>) }</ul>
+
+export default ({ notes }) => (
+  <ul>{ notes.map(note => 
+  <li key={ note.id }><Note task={ note.task }/></li>
+    ) }</ul>
 )
