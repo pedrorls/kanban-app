0cf51a24b76a981136133686003da5108009c049
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 18:18:01 -0200

    created notestore

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,17 @@
+import uuid from 'uuid';
+
+
+export default class NoteStore {
+  constructor() {
+    this.notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do Laundry'
+      }
+    ]
+  }
+}
