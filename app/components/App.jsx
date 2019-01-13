54664cb1f98c10ed13ffeaca91119cf6938673c8
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 20:26:00 -0200

    lanes resposible for notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,50 +1,70 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions'
+import Lanes from '../components/Lanes';
+import LaneActions from '../actions/LaneActions';
 
-
-class App extends Component{
-
-  addNote = () => {
-    this.props.NoteActions.create({
+const App = ({ LaneActions, lanes }) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New Task'
-    })
+      name: 'New Lane'
+    });
   }
 
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    this.props.NoteActions.delete(id)
-  }
-
-  activateNoteEdit = (id) => {
-    this.props.NoteActions.update({ id, editing: true });
-  }
-
-  editNote = (id, task) => {
-    this.props.NoteActions.update({ id, task, editing: false});
-  }
-
-  render() {
-    const { notes } = this.props;
-    return(
-      <div>
-        <button className="add-note" onClick={ this.addNote }>+</button>
-        <Notes 
-          notes={ notes }
-          onDelete={ this.deleteNote }
-          onNoteClick={ this.activateNoteEdit }
-          onEdit={ this.editNote }
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <button className='add-lane' onClick={ addLane }>+</button>
+      <Lanes lanes={ lanes }/>
+    </div>
+  )
 }
 
-export default connect(({ notes }) => ({
-  notes
+export default connect(({ lanes }) => ({
+  lanes
 }), {
-  NoteActions
+  LaneActions
 })(App)
+// class App extends Component{
+
+//   addNote = () => {
+//     this.props.NoteActions.create({
+//       id: uuid.v4(),
+//       task: 'New Task'
+//     })
+//   }
+
+//   deleteNote = (id, e) => {
+//     e.stopPropagation();
+//     this.props.NoteActions.delete(id)
+//   }
+
+//   activateNoteEdit = (id) => {
+//     this.props.NoteActions.update({ id, editing: true });
+//   }
+
+//   editNote = (id, task) => {
+//     this.props.NoteActions.update({ id, task, editing: false});
+//   }
+
+//   render() {
+//     const { notes } = this.props;
+//     return(
+//       <div>
+//         <button className="add-note" onClick={ this.addNote }>+</button>
+//         <Notes 
+//           notes={ notes }
+//           onDelete={ this.deleteNote }
+//           onNoteClick={ this.activateNoteEdit }
+//           onEdit={ this.editNote }
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default connect(({ notes }) => ({
+//   notes
+// }), {
+//   NoteActions
+// })(App)
