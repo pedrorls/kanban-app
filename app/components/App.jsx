4d319aa53676884d4944196a5f78941998bfba86
Pedro Silva <pedro.rls93@gmail.com>
2019-01-13 01:03:22 -0200

    separted in few files and can edit note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -37,12 +37,40 @@ export default class App extends Component{
     })
   }
 
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id){
+          note.editing = true;
+        }
+        return note;
+      })
+    });
+  }
+
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id ) {
+          note.editing = false;
+          note.task = task;
+        }
+        return note;
+      })
+    })
+  }
+
   render() {
     const { notes } = this.state;
     return(
       <div>
         <button onClick={ this.addNote }>+</button>
-        <Notes notes={ notes } onDelete={ this.deleteNote }/>
+        <Notes 
+          notes={ notes }
+          onDelete={ this.deleteNote }
+          onNoteClick={ this.activateNoteEdit }
+          onEdit={ this.editNote }
+        />
       </div>
     );
   }
