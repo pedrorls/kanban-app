aff4c27cbf9c2b761e4ad8affbe5992c9ff4daf4
Pedro Silva <pedro.rls93@gmail.com>
2019-01-17 18:55:16 -0200

    can drag note into a empty lane

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { compose } from 'redux';
+import { DropTarget } from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 import connect from '../libs/connect';
 import Notes from '../components/Notes';
 import NoteActions from '../actions/NoteActions';
@@ -6,7 +9,8 @@ import LaneActions from '../actions/LaneActions';
 import LaneHeader from './LaneHeader';
 
 
-const Lane = ({ lane, notes, NoteActions, LaneActions, ...props}) => {
+const Lane = ({ 
+  lane, notes, NoteActions, LaneActions, connectDropTarget, ...props}) => {
   const editNote = (id, task) => {
     NoteActions.update({ id, task, editing: false });
   }
@@ -24,7 +28,7 @@ const Lane = ({ lane, notes, NoteActions, LaneActions, ...props}) => {
     NoteActions.update({ id, editing: true });
   }
 
-  return (
+  return connectDropTarget(
     <div { ...props }>
       <LaneHeader lane={ lane }/>
       <Notes
@@ -37,16 +41,33 @@ const Lane = ({ lane, notes, NoteActions, LaneActions, ...props}) => {
   )
 }
 
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(!targetProps.lane.notes.length) {
+      LaneActions.attachToLane({
+        laneId: targetProps.lane.id,
+        noteId: sourceId
+      });
+    }
+  }
+}
+
 function selectNotesByIds(allNotes, noteIds = []) {
   return noteIds.reduce((notes, id) =>
     notes.concat(allNotes.filter(note => note.id === id)), [])
 }
 
-export default connect(
-  ({ notes }) => ({
+export default compose(
+  DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+  })),
+  connect(({ notes }) => ({
     notes
   }), {
     NoteActions,
     LaneActions
-  }
+  })
 )(Lane)
